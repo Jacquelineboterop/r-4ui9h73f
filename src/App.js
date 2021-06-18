35370751def0c8324d84f391554fa3a4d6fc55ef
Jacqueline <proyectos@branta.com.co>
2021-06-17 20:56:13 -0500

    first repeat

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={repeater : ''}
+    this.onChangeHandler=this.onChangeHandler.bind(this)
+  }
+  onChangeHandler(event){  
+    this.setState({ 
+     repeater: event.target.value     
+    })
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={ this.onChangeHandler }/>
+        <p className="repeater">{this.state.repeater}</p>
       </div>
     );
   }
